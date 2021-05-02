7e928a2ec663038d8824fd913d2a04a7b2f220fd
Clayton <Clayton@Claytons-MacBook-Pro-3.local>
2021-05-02 14:02:56 -0700

    Added scripting to write players data and tournaments data to S3 bucket

--- writeS3.py
+++ writeS3.py
@@ -1,2 +1,45 @@
 import pandas as pd
+import io
+from SportsDataPGA import SportsDataPGA
+import boto3
+from credentials import AWS_SECRET, AWS_ACCESS_KEY
 
+PGA = SportsDataPGA()
+
+players_df = PGA.getPlayers()
+tournaments_df = PGA.getTournaments()
+
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET
+)
+
+bucket = 'claytvh-personal-github'
+
+with io.StringIO() as csv_buffer:
+    players_df.to_csv(csv_buffer, index=False)
+
+    response = s3_client.put_object(
+        Bucket=bucket, Key="players.csv", Body=csv_buffer.getvalue()
+    )
+
+    status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+
+    if status == 200:
+        print("Successfully uploaded Player data to S3")
+    else:
+        print("Unsuccessfully uploaded Player data to S3")
+
+    tournaments_df.to_csv(csv_buffer, index=False)
+
+    response = s3_client.put_object(
+        Bucket=bucket, Key="tournaments.csv", Body=csv_buffer.getvalue()
+    )
+
+    status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+
+    if status == 200:
+        print("Successfully uploaded Tournament data to S3")
+    else:
+        print("Unsuccessfully uploaded Tournament data to S3")

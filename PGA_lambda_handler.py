10efe46648a364796331b719c55f433fc7030773
Clayton <Clayton@Claytons-MacBook-Pro-3.local>
2021-05-02 18:29:59 -0700

    Fixed credentials format for Lambda function, hid crentials for github, added docstring

--- PGA_lambda_handler.py
+++ PGA_lambda_handler.py
@@ -1,11 +1,21 @@
 import pandas as pd
+import json
 import boto3
-from credentials import AWS_SECRET, AWS_ACCESS_KEY
+import datetime
 
 
 def lambda_handler(event, context):
 
+    """Lambda function associated with
+        https://fr2srhyume.execute-api.us-west-2.amazonaws.com/demo/pga-geography endpoint"""
+
     bucket = 'claytvh-personal-github'
+    # AWS_ACCESS_KEY = credentials.AWS_ACCESS_KEY
+    # AWS_SECRET = credentials.AWS_SECRET
+
+    # Hide Credentials
+    AWS_ACCESS_KEY = ''
+    AWS_SECRET = ''
 
     s3_client = boto3.client(
         "s3",
@@ -13,27 +23,33 @@ def lambda_handler(event, context):
         aws_secret_access_key=AWS_SECRET,
     )
 
-    city = event['queryStringParameters']['City']
-    state = event['queryStringParameters']['State']
+    state = event['queryStringParameters']['state']
 
+    # Read players dataframe from S3
     players_response = s3_client.get_object(Bucket=bucket, Key="players.csv")
     players_df = pd.read_csv(players_response.get("Body"))
 
+    # Read tournaments data from S3
     tournaments_response = s3_client.get_object(Bucket=bucket, Key="tournaments.csv")
     tournaments_df = pd.read_csv(tournaments_response.get("Body"))
 
-    num_players = players_df[(players_df['City'] == city) & (players_df['State'] == state)]
-    num_tournaments = tournaments_df[(tournaments_df['City'] == city) & (tournaments_df['State'] == state)]
+    # Number of players and number of tournaments from the city and state passed as parameters
+    num_players = players_df[(players_df['BirthState'] == state)].shape[0]
+    num_tournaments = tournaments_df[(tournaments_df['State'] == state)].shape[0]
 
-    response = {
-        'city': city,
-        'state': state,
-        '# Golfers': num_players,
-        '# Tournaments': num_tournaments,
-    }
+    # Body of the response
+    responseBody = {}
+    responseBody['state'] = state
+    responseBody['num_players'] = num_players
+    responseBody['num_tournaments'] = num_tournaments
+    responseBody['timestamp'] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+    # Entire HTTP response object
+    responseObject = {}
+    responseObject['statusCode'] = 200
+    responseObject['headers'] = {}
+    responseObject['body'] = json.dumps(responseBody)
+
+    return responseObject
 
 
-#    city = event['queryStringParameters']['City']
- #   state = event['queryStringParameters']['State']
-
-lambda_handler(None,None)
